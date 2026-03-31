7b0516bbb39a3334f0b34a6f59faa17b32d14461
Jean <mikitenko@i.ua>
2026-03-31 15:38:10 +0300

    front contact

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('contact.contact', function ($view) {
+
+            $contact = [
+                'phone' => [
+                    '+38(050)123-45-56',
+                    '+38(080)123-45-56',
+                    '+38(090)123-45-56',
+                ],
+                'mail' => 'admin@mail.com',
+            ];
+
+            $view->with(['contact' => $contact]);
+        });
     }
 }
--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -6,11 +6,11 @@
                 <div class="fields">
                     <div class="field half">
                         <label for="name">Name</label>
-                        <input type="text" name="name" id="name" />
+                        <input type="text" name="name" id="name"/>
                     </div>
                     <div class="field half">
                         <label for="email">Email</label>
-                        <input type="text" name="email" id="email" />
+                        <input type="text" name="email" id="email"/>
                     </div>
                     <div class="field">
                         <label for="message">Message</label>
@@ -18,8 +18,8 @@
                     </div>
                 </div>
                 <ul class="actions">
-                    <li><input type="submit" value="Send Message" class="primary" /></li>
-                    <li><input type="reset" value="Clear" /></li>
+                    <li><input type="submit" value="Send Message" class="primary"/></li>
+                    <li><input type="reset" value="Clear"/></li>
                 </ul>
             </form>
         </section>
@@ -28,25 +28,40 @@
                 <div class="contact-method">
                     <span class="icon solid alt fa-envelope"></span>
                     <h3>Email</h3>
-                    <a href="#">information@untitled.tld</a>
+                    <a href="#">{{$contact['mail']}}</a>
                 </div>
             </section>
             <section>
                 <div class="contact-method">
                     <span class="icon solid alt fa-phone"></span>
                     <h3>Phone</h3>
-                    <span>(000) 000-0000 x12387</span>
+                    @foreach($contact['phone'] as $value)
+                        <a class="phone-link" href="tel:{{ preg_replace('/[^0-9+]/', '', $value) }}">
+                            {{ $value }}
+                        </a><br>
+                    @endforeach
                 </div>
             </section>
             <section>
                 <div class="contact-method">
                     <span class="icon solid alt fa-home"></span>
                     <h3>Address</h3>
-                    <span>1234 Somewhere Road #5432<br />
-										Nashville, TN 00000<br />
+                    <span>1234 Somewhere Road #5432<br/>
+										Nashville, TN 00000<br/>
 										United States of America</span>
                 </div>
             </section>
         </section>
     </div>
 </section>
+<style>
+    .phone-link {
+        border-bottom: none;
+        color: #fff;
+        transition: 0.2s;
+    }
+
+    .phone-link:hover {
+        text-decoration: underline; /* появляется только при наведении */
+    }
+</style>
